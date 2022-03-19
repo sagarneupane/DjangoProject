aa954bc9b3122e8fd604662c40face8ecd9649d1
sagar <cyber.crime252@gmail.com>
2022-03-19 21:05:07 +0545

    static file DjStatic

--- AuctionSite/settings.py
+++ AuctionSite/settings.py
@@ -128,8 +128,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -144,6 +144,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
     
 ]
+
+
 
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
 MEDIA_URL= "/media/"
